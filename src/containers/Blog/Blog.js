f4359ed76234eeaf8ccbf5312e5290d386ca5ceb
aswinks88 <aswinks88@gmail.com>
2020-02-14 00:40:19 +1300

    added link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -42,7 +42,15 @@ this.setState({selectedPostId: id})
             })
         }
         return (
-            <div>
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
