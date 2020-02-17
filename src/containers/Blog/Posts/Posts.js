476129f810e7132ae4479c25e8d5bf3d84e21a7d
aswinks88 <aswinks88@gmail.com>
2020-02-17 23:28:56 +1300

    added route parameters

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from 'axios'
 import Post from '../../../components/Post/Post'
 import './Posts.css'
+// import {Link} from 'react-router-dom'
 class Posts extends Component {
     state = {
         posts: []
@@ -24,16 +25,19 @@ class Posts extends Component {
         })
     }
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
+        // this.setState({selectedPostId: id})
+        this.props.history.push({pathname: '/' + id})
             }
     render(){
         let posts = <p style={{textAlign: 'center'}}>Something went wrong.......</p>
         if(!this.state.error){
             posts = this.state.posts.map(post => {
-                return <Post key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)}/>
+                return    <Post
+                    key={post.id}
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
             })
         }
         return(
